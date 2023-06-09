aa783b61290fa8c41fe5a162e0c07bda7f9477ab
Eric Camara <lordkim480@gmail.com>
2023-06-09 12:06:52 -0300

    refactor code structure

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { Bot } from './bot';
+
+export const bot = new Bot();
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './images';
+export * from './mongodb';
+export * from './tools';
